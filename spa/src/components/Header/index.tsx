c014688b945ae85518a3fd5f301d546ac0188890
emilyfinning <em@emilyjay.co.uk>
2022-05-20 17:34:07 +0100

    Basic auth with auth0

--- spa/src/components/Header/index.tsx
+++ spa/src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./styles.css";
 import Tab from "./Tab";
 
 function Header() {
   const { tabs, activeTab } = useSelector((state: any) => state.nav);
+
+  const { loginWithRedirect, logout, user } = useAuth0();
 
   return (
     <div className="header">
@@ -20,7 +23,11 @@ function Header() {
           ))}
         </div>
         <div className="header__user">
-          <div className="header__user-name">Emily Finning</div>
+          <div className="header__user-name">{user?.name}</div>
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+          </button>
           <img
             src={require("../../images/blank-profile.png")}
             alt="avatar"
--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { Auth0Provider } from "@auth0/auth0-react";
 import store from "./redux/store";
 
 import "./index.css";
@@ -11,6 +12,14 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_DOMAIN!}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID!}
+      redirectUri={window.location.origin}
+      audience="https://dev-v91vml4a.eu.auth0.com/api/v2/"
+      scope="read:current_user update:current_user_metadata"
+    >
+      <App />
+    </Auth0Provider>
   </Provider>
 );
